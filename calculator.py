f9b66b1c563e975861998d01322c4f78bf57372b
Alex Carranza <alexcarranza9889@gmail.com>
2026-09-08 20:05:23 -0600

    calculator progress

--- calculator.py
+++ calculator.py
@@ -1,6 +1,16 @@
-x = float(input("What's x? "))
-y = float(input("What's y? "))
+def main():
+    x = float(input("What's x? "))
+    y = float(input("What's y? "))
 
-z = round(x + y, 2)
+    print(square(x))
+    print(sum2nums(x,y))
 
-print(z)
+def sum2nums(x, y):
+    z = round(x + y, 2)
+    return z
+
+def square(n):
+    n = float(n)
+    return n**2
+
+main()

d6b7c0ded6138ecc60d22df7b5d7c91dc10f2b99
Alex Carranza <alexcarranza9889@gmail.com>
2026-09-08 19:41:19 -0600

    more str exercises

--- hello.py
+++ hello.py
@@ -1,5 +1,14 @@
 # Ask users for their name
-name = input("What's your name? ")
+# Remove white spaces and capitalize user's name
+name = input("What's your name? ").strip().title()
 
-# Say hello to the users
-print("Hello,", name)
+print(f"Hello, {name}")
+
+# Split user's name into first name and last name
+first, last = name.split(" ")
+print(f"Hello, {first} {last}")
+
+
+# obj = {"Alex","eats","pasta"}
+obj = ["Alex","eats","pasta"]
+print(*obj, sep=' ', end='\n')

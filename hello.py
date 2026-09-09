63cc1bc9079a5bff73c4ec4e701c371c8ccac355
Alex Carranza <alexcarranza9889@gmail.com>
2026-09-08 19:59:33 -0600

    more updates

--- hello.py
+++ hello.py
@@ -1,14 +1,20 @@
-# Ask users for their name
-# Remove white spaces and capitalize user's name
-name = input("What's your name? ").strip().title()
+def main():
+    name = input("What's your name? ").strip().title()
+    hello(name)
 
-print(f"Hello, {name}")
+# Ask users for their name
+def hello(to="Alex"):
+    # Remove white spaces and capitalize user's name
+
+    print(f"Hello, {to}")
 
 # Split user's name into first name and last name
-first, last = name.split(" ")
-print(f"Hello, {first} {last}")
+# first, last = name.split(" ")
+# print(f"Hello, {first} {last}")
 
 
 # obj = {"Alex","eats","pasta"}
-obj = ["Alex","eats","pasta"]
-print(*obj, sep=' ', end='\n')
+# obj = ["Alex","eats","pasta"]
+# print(*obj, sep=' ', end='\n')
+
+main()
